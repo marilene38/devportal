ad7dc7d4522ab88f7628e6b396ea93b00351577e
Larkin Young <larkin.young@consensys.net>
2024-07-04 09:05:10 +0300

    Modified CSS global styles and config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,169 +1,176 @@
-import { defineConfig } from "astro/config";
-import starlight from "@astrojs/starlight";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
+import remarkSlug from 'remark-slug';
+import remarkAutolinkHeadings from 'remark-autolink-headings';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: "Algorand Developer Portal",
-			logo: {
-				light: "/src/assets/images/portal-logo-light-mode.svg",
-				dark: "/src/assets/images/portal-logo-dark-mode.svg",
-				alt: "Algorand Developer Portal",
-				replacesTitle: true,
-			}, 
-			favicon: "/favicon.png",
-			social: {
-				github: "https://github.com/withastro/starlight",
-			},
-			customCss: ["/src/styles/global.css"],
-			sidebar: [
-				{
-					label: "Build",
-					items: [
-						{
-							label: "AlgoKit",
-							collapsed: true,
-							autogenerate: {
-								directory: "build/algokit",
-							},
-						},
-						{
-							label: "Accounts",
-							collapsed: true,
-							autogenerate: {
-								directory: "build/assets",
-							},
-						},
-						{
-							label: "Assets",
-							collapsed: true,
-							autogenerate: {
-								directory: "build/assets",
-							},
-						},
-						{
-							label: "Transactions",
-							collapsed: true,
-							autogenerate: {
-								directory: "build/transactions",
-							},
-						},
-						{
-							label: "Smart Contracts",
-							collapsed: true,
-							autogenerate: {
-								directory: "build/smart-contracts",
-							},
-						},
-					],
-				},
-				{
-					label: "Algorand Protocol",
-					collapsed: true,
-					badge: {
-						text: "todo",
-						variant: "danger",
-					},
-					items: [
-						{
-							label: "Consensus",
-							badge: {
-								text: "todo",
-								variant: "danger",
-							},
-							autogenerate: {
-								directory: "protocol/consensus",
-							},
-						},
-						{
-							label: "Node",
-							badge: {
-								text: "todo",
-								variant: "danger",
-							},
-							autogenerate: {
-								directory: "protocol/node",
-							},
-						},
-						{
-							label: "APIs",
-							badge: {
-								text: "todo",
-								variant: "danger",
-							},
-							autogenerate: {
-								directory: "protocol/api",
-							},
-						},
-						{
-							label: "State Proofs",
-							badge: {
-								text: "todo",
-								variant: "danger",
-							},
-							autogenerate: {
-								directory: "protocol/state-proofs",
-							},
-						},
-						{
-							label: "Features",
-							badge: {
-								text: "todo",
-								variant: "danger",
-							},
-							autogenerate: {
-								directory: "protocol/features",
-							},
-						},
-						{
-							label: "AVM",
-							badge: {
-								text: "todo",
-								variant: "danger",
-							},
-							autogenerate: {
-								directory: "protocol/avm",
-							},
-						},
-					],
-				},
-				{
-					label: "Reference",
-					collapsed: true,
-					badge: {
-						text: "todo",
-						variant: "danger",
-					},
-					autogenerate: {
-						directory: "reference",
-					},
-				},
-				{
-					label: "Running a Node",
-					collapsed: true,
-					badge: {
-						text: "todo",
-						variant: "danger",
-					},
-					autogenerate: {
-						directory: "node_running",
-					},
-				},
-				{
-					label: "Security & Best Practices",
-					collapsed: true,
-					badge: {
-						text: "todo",
-						variant: "danger",
-					},
-					autogenerate: {
-						directory: "security",
-					},
-				},
-			],
-		}),
-		tailwind({ applyBaseStyles: false }),
-	],
+  integrations: [
+    starlight({
+      title: 'Algorand Developer Portal',
+      logo: {
+        light: '/src/assets/images/portal-logo-light-mode.svg',
+        dark: '/src/assets/images/portal-logo-dark-mode.svg',
+        alt: 'Algorand Developer Portal',
+        replacesTitle: true,
+      },
+      favicon: '/favicon.png',
+      social: {
+        github: 'https://github.com/withastro/starlight',
+      },
+      customCss: ['/src/styles/global.css'],
+      sidebar: [
+        {
+          label: 'Build',
+          items: [
+            {
+              label: 'AlgoKit',
+              collapsed: true,
+              autogenerate: { directory: 'build/algokit' },
+            },
+            {
+              label: 'Accounts',
+              collapsed: true,
+              autogenerate: {
+                directory: 'build/assets',
+              },
+            },
+            {
+              label: 'Assets',
+              collapsed: true,
+              autogenerate: {
+                directory: 'build/assets',
+              },
+            },
+            {
+              label: 'Transactions',
+              collapsed: true,
+              autogenerate: {
+                directory: 'build/transactions',
+              },
+            },
+            {
+              label: 'Smart Contracts',
+              collapsed: true,
+              autogenerate: {
+                directory: 'build/smart-contracts',
+              },
+            },
+          ],
+        },
+        {
+          label: 'Protocol',
+          collapsed: true,
+          badge: {
+            text: 'todo',
+            variant: 'danger',
+          },
+          items: [
+            {
+              label: 'Consensus',
+              badge: {
+                text: 'todo',
+                variant: 'danger',
+              },
+              autogenerate: {
+                directory: 'protocol/consensus',
+              },
+            },
+            {
+              label: 'Node',
+              badge: {
+                text: 'todo',
+                variant: 'danger',
+              },
+              autogenerate: {
+                directory: 'protocol/node',
+              },
+            },
+            {
+              label: 'APIs',
+              badge: {
+                text: 'todo',
+                variant: 'danger',
+              },
+              autogenerate: {
+                directory: 'protocol/api',
+              },
+            },
+            {
+              label: 'State Proofs',
+              badge: {
+                text: 'todo',
+                variant: 'danger',
+              },
+              autogenerate: {
+                directory: 'protocol/state-proofs',
+              },
+            },
+            {
+              label: 'Features',
+              badge: {
+                text: 'todo',
+                variant: 'danger',
+              },
+              autogenerate: {
+                directory: 'protocol/features',
+              },
+            },
+            {
+              label: 'AVM',
+              badge: {
+                text: 'todo',
+                variant: 'danger',
+              },
+              autogenerate: {
+                directory: 'protocol/avm',
+              },
+            },
+          ],
+        },
+        {
+          label: 'Reference',
+          collapsed: true,
+          badge: {
+            text: 'todo',
+            variant: 'danger',
+          },
+          autogenerate: {
+            directory: 'reference',
+          },
+        },
+        {
+          label: 'Node Running',
+          collapsed: true,
+          badge: {
+            text: 'todo',
+            variant: 'danger',
+          },
+          autogenerate: {
+            directory: 'node_running',
+          },
+        },
+      ],
+    }),
+    tailwind({ applyBaseStyles: false }),
+  ],
+  markdown: {
+    remarkPlugins: [
+      remarkSlug,
+      [
+        remarkAutolinkHeadings,
+        {
+          behavior: 'append',
+          linkProperties: {
+            className: ['anchor'],
+          },
+          content: {
+            type: 'text',
+            value: '#',
+          },
+        },
+      ],
+    ],
+  },
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,21 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontSize: { 
+        xs: '0.75rem',  // 12px
+        sm: '0.875rem', // 14px
+        base: '1rem',   // 16px
+        lg: '1.125rem', // 18px
+        xl: '1.25rem',  // 20px
+        '2xl': '1.5rem',// 24px
+        '3xl': '1.875rem', // 30px
+        '4xl': '2.25rem', // 36px
+        '5xl': '3rem', // 48px
+        '6xl': '3.75rem', // 60px
+        '7xl': '4.5rem', // 72px
+        '8xl': '6rem', // 96px
+        '9xl': '8rem', // 128px
+      },
       fontFamily: {
         sans: ["Aeonik", "sans-serif"], // Set Aeonik as the default sans font
       },
@@ -36,8 +51,15 @@ export default {
           900: "#134d4e",
           950: "#042c2f",
         },
+        code: {
+          bg: "#282c34", // Custom background color for code blocks
+          text: "#3f3a4e", // Custom text color for code blocks
+          keyword: "#c678dd", // Custom color for keywords
+          string: "#98c379", // Custom color for strings
+        },
         accent: colors.indigo,
         gray: colors.zinc,
+        teal: "#17CAC6",
       },
     },
   },
