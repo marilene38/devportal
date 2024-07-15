e5814b9d2955298d41db58bb6237b74372bea305
Larkin Young <larkin.young@consensys.net>
2024-07-15 11:35:43 +0300

    Updated reference docs for python utils

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,7 @@ export default {
   theme: {
     extend: {
       fontSize: {
+        xxs: '0.625rem', // 10px
         xs: '0.75rem', // 12px
         sm: '0.875rem', // 14px
         base: '1rem', // 16px
