c8e619ffc6e6d4a8460f17b106fb0c6c6117d5c2
Larkin Young <larkin.young@consensys.net>
2024-07-16 17:33:22 +0300

    Added global banner

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,16 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
 import { docsSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
-	docs: defineCollection({ schema: docsSchema() }),
+  docs: defineCollection({
+    schema: docsSchema({
+      extend: z.object({
+        // Add a default value to the built-in `banner` field.
+        banner: z.object({ content: z.string() }).default({
+          content:
+            'These shiny new docs are under active construction; please bear with us as we work! For complete documentation, please refer to our <a href="https://developer.algorand.org/docs/" target="_blank" rel="noopener">old developer portal</a>.',
+        }),
+      }),
+    }),
+  }),
 };
