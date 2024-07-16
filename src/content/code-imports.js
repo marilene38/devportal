da891f4ddf560c4bfafb48e6473402bae3c68745
Larkin Young <larkin.young@consensys.net>
2024-07-16 17:29:18 +0300

    Fixed typescript code example errors

--- src/content/code-imports.js
+++ src/content/code-imports.js
@@ -1,11 +1,11 @@
 // src/codeImports.js
-import createGlobalStateTEAL from '/src/examples/storage-global-init.approval.teal?raw';
-import getGlobalStorageTEAL from '/src/examples/storage-global-get.approval.teal?raw';
-import setGlobalStorageTEAL from '/src/examples/storage-global-set.approval.teal?raw';
-import deleteGlobalStorageTEAL from '/src/examples/storage-global-delete.approval.teal?raw';
-import getLocalStorageTEAL from '/src/examples/storage-local-get.approval.teal?raw';
-import setLocalStorageTEAL from '/src/examples/storage-local-set.approval.teal?raw';
-import deleteLocalStorageTEAL from '/src/examples/storage-local-delete.approval.teal?raw';
+import createGlobalStateTEAL from '/examples/storage-global-init.approval.teal?raw';
+import getGlobalStorageTEAL from '/examples/storage-global-get.approval.teal?raw';
+import setGlobalStorageTEAL from '/examples/storage-global-set.approval.teal?raw';
+import deleteGlobalStorageTEAL from '/examples/storage-global-delete.approval.teal?raw';
+import getLocalStorageTEAL from '/examples/storage-local-get.approval.teal?raw';
+import setLocalStorageTEAL from '/examples/storage-local-set.approval.teal?raw';
+import deleteLocalStorageTEAL from '/examples/storage-local-delete.approval.teal?raw';
 
 export {
   createGlobalStateTEAL,
