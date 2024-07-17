f3489e3d491fbcebda9b645f2bce34735d32eccf
Larkin Young <larkin.young@consensys.net>
2024-07-17 13:16:05 +0300

    Updated EC config and tsconfig.json

--- ec.config.mjs
+++ ec.config.mjs
@@ -1,11 +1,9 @@
 import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers';
-import { flavors } from '@catppuccin/palette';
 import fs from 'node:fs';
 
 /** @type {import('@astrojs/starlight/expressive-code').StarlightExpressiveCodeOptions} */
 export default {
   plugins: [pluginLineNumbers()],
-  // themes: [flavors.latte, flavors.mocha],
   themes: ['material-theme'],
   shiki: {
     langs: [JSON.parse(fs.readFileSync('src/utils/teal-syntax.json', 'utf-8'))],
