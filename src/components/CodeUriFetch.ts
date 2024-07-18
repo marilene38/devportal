a01d4561ee8d8899b50c88e422b5d44eae85076f
Larkin Young <larkin.young@consensys.net>
2024-07-18 10:36:01 +0300

    Updated CodeUri component

---
+++ src/components/CodeUriFetch.ts
@@ -0,0 +1,46 @@
+export async function fetchAndExtract(
+  uri: string,
+  lines: string | undefined,
+): Promise<string> {
+  const linesRegex = /^(\d+(-\d+)?)(,\d+(-\d+)?)*$/;
+  if (lines && !linesRegex.test(lines)) {
+    throw new Error('CodeUri: Invalid lines format.');
+  }
+
+  const code = await fetch(uri).then(r => {
+    if (!r.ok) {
+      throw new Error(
+        `CodeUri: Failed to fetch code from ${uri}: ${r.statusText}`,
+      );
+    }
+    return r.text();
+  });
+
+  const codeLines = code.split('\n');
+  let selectedLines: string[] = [];
+  if (lines) {
+    lines.split(',').forEach((part: string) => {
+      if (part.includes('-')) {
+        const [start, end] = part.split('-').map(Number);
+        if (start < 1 || start > end) {
+          throw new Error('CodeUri: Invalid start or end line.');
+        }
+        selectedLines.push(...codeLines.slice(start - 1, end));
+      } else {
+        const line = Number(part);
+        if (line < 1) {
+          throw new Error('CodeUri: Invalid line number.');
+        }
+        selectedLines.push(codeLines[line - 1]);
+      }
+
+      selectedLines.push('');
+    });
+
+    selectedLines.pop();
+  } else {
+    selectedLines = codeLines;
+  }
+
+  return selectedLines.join('\n');
+}
