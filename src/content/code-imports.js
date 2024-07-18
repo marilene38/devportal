b581b19338b407410469cb71a62ca78afc3e5ebe
Larkin Young <larkin.young@consensys.net>
2024-07-18 14:17:34 +0300

    Moved code examples into standard structure

--- src/content/code-imports.js
+++ src/content/code-imports.js
@@ -1,11 +1,11 @@
 // src/codeImports.js
-import createGlobalStateTEAL from '/examples/storage-global-init.approval.teal?raw';
-import getGlobalStorageTEAL from '/examples/storage-global-get.approval.teal?raw';
-import setGlobalStorageTEAL from '/examples/storage-global-set.approval.teal?raw';
-import deleteGlobalStorageTEAL from '/examples/storage-global-delete.approval.teal?raw';
-import getLocalStorageTEAL from '/examples/storage-local-get.approval.teal?raw';
-import setLocalStorageTEAL from '/examples/storage-local-set.approval.teal?raw';
-import deleteLocalStorageTEAL from '/examples/storage-local-delete.approval.teal?raw';
+import createGlobalStateTEAL from '/examples/smart_contracts/teal/storage-global-init.approval.teal?raw';
+import getGlobalStorageTEAL from '/examples/smart_contracts/teal/storage-global-get.approval.teal?raw';
+import setGlobalStorageTEAL from '/examples/smart_contracts/teal/storage-global-set.approval.teal?raw';
+import deleteGlobalStorageTEAL from '/examples/smart_contracts/teal/storage-global-delete.approval.teal?raw';
+import getLocalStorageTEAL from '/examples/smart_contracts/teal/storage-local-get.approval.teal?raw';
+import setLocalStorageTEAL from '/examples/smart_contracts/teal/storage-local-set.approval.teal?raw';
+import deleteLocalStorageTEAL from '/examples/smart_contracts/teal/storage-local-delete.approval.teal?raw';
 
 export {
   createGlobalStateTEAL,
