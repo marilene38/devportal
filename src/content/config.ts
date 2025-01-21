a390d4b444ad9c40650a4d4db19350bf6a7f6f85
Astha Yadav <75947851+Asthay97@users.noreply.github.com>
2025-01-21 11:52:45 +0530

    Updated landing page message to inform about node docs

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,7 +8,7 @@ export const collections = {
         // Add a default value to the built-in `banner` field.
         banner: z.object({ content: z.string() }).default({
           content:
-            'These shiny new docs are under active construction; please bear with us as we work! For complete documentation, please refer to our <a href="https://developer.algorand.org/docs/" target="_blank" rel="noopener">old developer portal</a>.',
+            'These shiny new docs are under active construction. Currently, only Node-related documentation is available, but more updates will follow as we continue improving the site. For complete documentation, please refer to our <a href="https://developer.algorand.org/docs/" target="_blank" rel="noopener">old developer portal</a>.',
         }),
       }),
     }),
