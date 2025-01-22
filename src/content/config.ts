f0979ff2751614b0ffcc7acd67e2230cb1a8e4c3
Brian Whippo <brian.whippo@algorand.foundation>
2025-01-21 21:06:56 -0500

    Landing page & minor review fixes

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,7 +8,7 @@ export const collections = {
         // Add a default value to the built-in `banner` field.
         banner: z.object({ content: z.string() }).default({
           content:
-            'Our new developer portal is under construction. Currently, only docs related to running a node on Algorand are available. For complete documentation, please refer to our <a href="https://developer.algorand.org/docs/" target="_blank" rel="noopener">old developer portal</a>.',
+            'This new developer portal is under construction. For complete documentation, please refer to the <a href="https://developer.algorand.org/docs/" target="_blank" rel="noopener">old developer portal</a>.',
         }),
       }),
     }),
