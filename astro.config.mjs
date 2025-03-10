7c0cf7085cdebdb95e6938358f1ac5943cde142b
Marco Podien <marco.podien@algorand.foundation>
2025-03-10 14:02:03 +0100

    wip - feedback: structural adaptations

--- astro.config.mjs
+++ astro.config.mjs
@@ -169,41 +169,19 @@ export default defineConfig({
                   badge: 'WIP',
                 },
                 {
-                  label: 'Creating Assets',
-                  link: 'concepts/assets/create',
-                },
-                {
-                  label: 'Opting In & Out',
-                  link: 'concepts/assets/opt-in-out',
+                  label: 'Asset Operations',
+                  link: 'concepts/assets/asset-operations',
                   badge: 'WIP',
                 },
                 {
-                  label: 'Updating Assets',
-                  link: 'concepts/assets/update',
-                  badge: 'WIP',
-                },
-                {
-                  label: 'Transferring Assets',
-                  link: 'concepts/assets/transfer',
-                },
-                {
-                  label: 'Deleting Assets',
-                  link: 'concepts/assets/delete',
-                  badge: 'WIP',
-                },
-                {
-                  label: 'Clawing Back Assets',
-                  link: 'concepts/assets/clawback',
-                  badge: 'WIP',
-                },
-                {
-                  label: 'Freezing Assets',
-                  link: 'concepts/assets/freeze',
+                  label: 'Assets Metadata',
+                  link: 'concepts/assets/asset-metadata',
                   badge: 'WIP',
                 },
                 {
                   label: 'Known Assets',
                   link: 'concepts/assets/known-assets',
+                  badge: 'WIP',
                 },
               ],
             },
