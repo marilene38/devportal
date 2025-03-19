98288a5edebd22fafbdb72b5369a357fedd41d98
Larkin Young <larkin.young@algorand.foundation>
2025-03-19 14:28:32 +0000

    Fix Kapa AI image path

--- astro.config.mjs
+++ astro.config.mjs
@@ -46,7 +46,7 @@ export default defineConfig({
             'data-website-id': '6e799942-b20a-4203-8103-93582a2611e1',
             'data-project-name': 'Algorand',
             'data-project-color': '#2CB7BC',
-            'data-project-logo': '/src/assets/images/algorand-logo.png',
+            'data-project-logo': '/algorand-logo.png',
           },
         },
       ],
